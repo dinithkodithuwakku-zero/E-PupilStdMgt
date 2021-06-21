b1de17480eafcadffdd13b7e09ade483aa0efd51
Dinith Sadeepa Kodithuwakku <dinith.kodithuwakku@gmail.com>
2021-06-21 17:15:53 +0530

    Initialize Services

---
+++ E-PupilStdMgt/src/service/custom/IStudentServiceCustom.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_PupilStdMgt.src.service.custom
+{
+    interface IStudentServiceCustom: ISuperService
+    {
+    }
+}
---
+++ E-PupilStdMgt/src/service/custom/impl/StudentServiceImpl.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_PupilStdMgt.src.service.custom.impl
+{
+    class StudentServiceImpl: IStudentServiceCustom
+    {
+    }
+}
